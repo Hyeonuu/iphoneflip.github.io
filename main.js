81f10216a91adc6d28b97bc707cbb88a55d0918c
Nenu <109052038+Hyeonuu@users.noreply.github.com>
2025-02-09 18:08:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
     observer.observe(section02);
 });
 
-// section03
+// section03 efect
 document.addEventListener('DOMContentLoaded', function() {
     const section03 = document.querySelector('#section03');
     const section03text01 = document.querySelector('.section03-text01');
@@ -80,3 +80,52 @@ document.addEventListener('DOMContentLoaded', function() {
     // section03를 관찰 대상으로 등록
     observer.observe(section03);
 });
+
+//section04-Text
+
+const section04text01 = document.querySelector('.section04-text01');
+const section04text02 = document.querySelector('.section04-text02');
+
+const observerText = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.style.opacity = 1;
+        }
+    });
+},{threshold: 0.5});
+
+observerText.observe(section04text01);
+observerText.observe(section04text02);
+
+//section04-video
+const video = document.querySelector('.section04-video');
+
+// 이미 재생된 동영상인지 확인하는 변수
+let hasPlayed = false;
+
+// 동영상의 loop 속성 제거
+video.loop = false;
+
+// IntersectionObserver 콜백 함수 정의
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting && !hasPlayed) {
+            // 동영상이 화면에 보이고 아직 재생되지 않았다면 재생 시작
+            entry.target.play();
+            entry.target.playbackRate =  0.7;
+            hasPlayed = true;  // 동영상이 재생되었음을 표시
+        }
+        
+        // 동영상이 화면에 보이면 서서히 나타나게 함
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1; // 화면에 보이면 opacity를 1로 변경
+        } else {
+            entry.target.style.opacity = 0; // 화면에서 벗어나면 opacity를 0으로 변경
+        }
+    });
+}, {
+    threshold: 0.5 // 화면에 50% 이상 보일 때 동작 시작
+});
+
+// 동영상 요소를 옵저버에 등록합니다.
+observer.observe(video);
